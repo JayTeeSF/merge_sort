b46ff04de53e9ff1d5c3ddab9d5b7e5690dc9e71
Jonathan Thomas <jonathan@zoodles.com>
2012-04-22 20:09:13 -0700

    first draft merge-sort

---
+++ merge_sort.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+module Sorter
+  extend self
+
+  def sort(list=[])
+    puts "sorting: #{list.inspect}"
+    gets
+    list_size = list.size
+    return list if list_size <= 1
+    puts "still going"
+
+    half = list_size/2
+    #pivot = list[rand(list_size)]
+    #list[rand(list_size)] # something like this -- may be quicksort though ;-)
+    #pivot_index = list.index(pivot)
+    if 2 == list_size
+      beginning = [list[0]]
+      ending = [list[1]]
+    else
+      beginning = list[0..(half -1)]
+      ending = list[half..-1]
+    end
+    merge(sort(beginning), sort(ending))
+  end
+
+  def merge(a, b)
+    result = [] 
+    a_index = 0
+    a_size = a.size
+    b_index = 0
+    b_size = b.size
+    full_size = a_size + b_size
+    0.upto(full_size - 1) do |e|
+      if (a_index < a_size && ((b_index >= b_size) || (a[a_index] <= b[b_index]) ))
+        result << a[a_index]
+        a_index += 1
+      else
+        result << b[b_index]
+        b_index += 1
+      end
+    end
+    #a.each_with_index do |e,i|
+    #  result += (e <= b[i] ? [e, b[i]] : [b[i], e])
+    #end
+    puts "returning: #{result.inspect}"
+    result
+  end
+
+end
+puts Sorter.sort([5])
+puts Sorter.sort([5,4,1,8,7,2,9,6,3])
